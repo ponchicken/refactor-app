8fd73a422faf2acb981ab95dd9f23933ec91aaf3
Alexander Balandin <al.balandin@corp.vk.com>
2023-11-20 16:29:04 +0300

    Add base webpack config

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+
+const plugins = [];
+
+if (process.env.NODE_ENV === 'development') {
+  plugins.push('react-refresh/babel');
+}
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins,
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import _ from "lodash";
-import "./styles.scss";
+import "./styles.css";
 
 /**
  * @see https://thecatapi.com/
